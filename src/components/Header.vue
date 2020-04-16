5a87445c6d38d38f251b793017fc328d935227d4
Rattling_shiba_inu <43816939+fish-inu@users.noreply.github.com>
2020-04-16 16:38:05 +0800

    Create Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-menu
+    default-active="1"
+    class="el-menu-demo"
+    mode="horizontal"
+    active-text-color="#67C23A"
+  >
+    <el-menu-item index="1" @click="$router.push('/')">
+      Home
+    </el-menu-item>
+    <el-menu-item index="2" @click="$router.push('/about')"
+      >About</el-menu-item
+    >
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
